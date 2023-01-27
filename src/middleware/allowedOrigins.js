677f45bfc551f5d754d2496dbebefd45ef9a3622
Aimeana100 <aimeanathole@gmail.com>
2023-01-27 17:34:13 +0200

    bg(fix make test-coverage): remove test coverage in ci/cd , github workflow

--- src/middleware/allowedOrigins.js
+++ src/middleware/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
     'http://127.0.0.1:5000',
-    'http://localhost:5000',
-    'https://indigo-barracuda-wig.cyclic.app/'
+    'https://indigo-barracuda-wig.cyclic.app'
 ];
 
 export default allowedOrigins;
