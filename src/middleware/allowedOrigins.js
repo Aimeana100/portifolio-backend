6de3e9bf22db788769655e7356f5e195e8dd1b00
Aimeana100 <aimeanathole@gmail.com>
2023-01-27 18:04:04 +0200

    bg(fix typo error in auth controller)

--- src/middleware/allowedOrigins.js
+++ src/middleware/allowedOrigins.js
@@ -1,6 +1,9 @@
 const allowedOrigins = [
+    'https://indigo-barracuda-wig.cyclic.app/',
+    'http://127.0.0.1:5500',
     'http://127.0.0.1:5000',
-    'https://indigo-barracuda-wig.cyclic.app'
+    'http://localhost:5000',
+
 ];
 
 export default allowedOrigins;
