412048b0697dd22f0921e731af4f84851c3cfefa
Aimeana100 <aimeanathole@gmail.com>
2023-02-05 23:18:00 +0200

    ft(add-allowed-orgigin): addedd https://karinganire-anathole.netlify.app/ to the allowed origin to frot integration purpose

--- src/middleware/allowedOrigins.js
+++ src/middleware/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'https://indigo-barracuda-wig.cyclic.app/',
-    'http://127.0.0.1:5500',
+    'https://karinganire-anathole.netlify.app/',
     'http://127.0.0.1:5000',
     'http://localhost:5000',
 
